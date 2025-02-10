bffe862739a1b04b8bbdf01b607be777257737b5
AnasRouam <114496477+AnasRouam@users.noreply.github.com>
2025-02-10 12:29:58 +0100

    testing github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://anasrouam.github.io',
+  base: '/AnasRouam.github.io/',
+  output: 'static'
+});
